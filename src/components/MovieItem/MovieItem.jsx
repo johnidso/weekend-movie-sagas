bae32fa95e3a1d9af91c45105ef5c7f9cff48619
John Idso <john.d.idso@gmail.com>
2021-07-24 12:47:04 -0500

    set up MovieItem component to handle single movie data

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,17 +1,28 @@
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 function MovieItem (props) {
     const { id } = useParams();
+    const dispatch = useDispatch();
+    const movies = useSelector(store => store.movies);
+
     // make a GET saga I can dispatch against to get specific movie info from an ID
+    useEffect(() => {
+        dispatch({type:'GET_MOVIE', payload: id});
+    }, []);
+    
+    
     return(
         <>
             {/* props might not be the way to go here - RESEARCH */}
-            <h1>{props.title}</h1> 
-            <img src={props.poster} />
-            <p>{props.description}</p>
+            <h1>{movies.title}</h1> 
+            <img src={movies.poster} />
+            <p>{movies.description}</p>
+            <h2>Genres</h2>
             <ul>
                 {/* iterates over genres present */}
-                {props.genres.map(genre =>{
+                {movies.genre_name.map(genre =>{
                     return <li>{genre}</li>
                 })}
             </ul>

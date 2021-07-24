a3b5196f12d3c80c9ce472f86a87121eaa8783a4
John Idso <john.d.idso@gmail.com>
2021-07-24 14:22:32 -0500

    updated genre router with get query

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+  const query = `SELECT * FROM genres ORDER BY "id" ASC;`;
+  pool.query(query)
+    .then( result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('error getting genres', err);
+      res.sendStatus(500)
+    })
 });
 
 module.exports = router;
---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,6 @@
+const { useState } = require("react");
+
+
+function AddMovie () {
+    const [newMovie, setNewMovie] = useState({});
+}
